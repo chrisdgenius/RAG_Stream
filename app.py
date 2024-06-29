48d1812ca327c4f841544014552bc0a046c769b3
Christian Okonta <okonta.christian@yahoo.com>
2024-06-29 13:11:21 +0000

    feat:demo with streamlit

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
-import panda as pd
+import pandas as pd
+
+st.write(" The is the first streamlit web python app")
+st.write (pd.DataFrame(
+    {
+        'first column': [1, 2, 3, 4, 5],
+        'second column': [10, 20, 30, 40, 50]
+    }
+))
+
+option=st.selectbox("how do you want to be contacted",
+                    ('Email', 'phone'))
+
+st.write("You selected", option)
+                    
 
 st.write("Hello world")
